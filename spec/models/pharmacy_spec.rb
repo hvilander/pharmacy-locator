68188931163a743275def61b27276530a3a3ad19
Harold <harold.vilander@gmail.com>
2020-11-15 11:12:41 -0600

    added pharmacy model tests

--- spec/models/pharmacy_spec.rb
+++ spec/models/pharmacy_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Pharmacy, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#get_nearest_pharmacy' do
+    context 'there is a nearest pharmacy found' do
+      it 'returns the nearest pharmacy' do
+        expected_name = "TALLGRASS PHARMACY"
+        expected_distance = 1.0
+
+        nearest_pharmacy = Pharmacy.get_nearest_pharmacy(39.05716000, -95.76692000)
+
+        expect(nearest_pharmacy).to_not be_nil
+        expect(nearest_pharmacy.name).to eq(expected_name);
+      end
+
+    end
+
+    context 'no pharmacy can be found' do
+      before(:each) do
+        allow(Pharmacy).to receive(:all).and_return(Pharmacy.none)
+      end
+
+      it 'returns nil if a pharmacy could not be found' do
+        nearest_pharmacy = Pharmacy.get_nearest_pharmacy(39.05716000, -95.76692000)
+        expect(nearest_pharmacy).to eq(nil)
+      end
+    end
+  end
+
+
 end
